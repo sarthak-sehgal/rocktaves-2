57a1d534336add9d38ed24515a562aa9bdbbc3dd
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2017-10-29 10:52:18 +0530

    contact tab added, nav hover effect changed

--- script.js
+++ script.js
@@ -3,18 +3,11 @@ $(document).ready(function(){
 
     $(".title").hide(0).delay(100).fadeIn(1000);
 
-    setTimeout( function(){
+    setTimeout(function(){
     	$(".oasislogo").css('opacity','1');
    },700);
 
-   //  $(".oasislogo").hover(function (){
-   //  	$(".oasistext").stop().animate({'opacity':'1'},200);
-   //  }, function (){
-   //  	$(".oasistext").stop().animate({'opacity':'0.5'},200);
-   //  });
-
-
-	$(".nav-desc, .nav-rules").click(function(){
+	$(".nav-desc, .nav-rules, .nav-contact").click(function(){
 		$('.title').animate({ marginTop: '300px', opacity:'0' }, 500);
 		setTimeout( function(){
 		$('.title').css('display','none');
@@ -22,6 +15,14 @@ $(document).ready(function(){
 	});
 	
 	$(".nav-desc").click(function(){
+		$('.nav-home, .nav-rules, .nav-contact').css({
+			'background-color' : 'rgba(255,255,255,0.7)',
+			'color' : '#000',
+		});
+		$('.nav-desc').css({
+			'background-color' : 'rgba(0,0,0,0.7)',
+			'color' : '#fff',
+		});
 		$('.desc-box').css('display', 'flex');
 		$('.desc-box').animate({ marginTop: '4vh', opacity:'1' }, 750);
 
@@ -29,9 +30,21 @@ $(document).ready(function(){
 		setTimeout( function(){
 		$('.rules-box').css('display','none');
 		   },500);
+		$('.contact-box').animate({ marginTop: '300px', opacity:'0' }, 500);
+		setTimeout( function(){
+		$('.contact-box').css('display','none');
+		   },500);
 	});
 
 	$(".nav-rules").click(function(){
+		$('.nav-home, .nav-desc, .nav-contact').css({
+			'background-color' : 'rgba(255,255,255,0.7)',
+			'color' : '#000',
+		});
+		$('.nav-rules').css({
+			'background-color' : 'rgba(0,0,0,0.7)',
+			'color' : '#fff',
+		});
 		$('.rules-box').css('display', 'flex');
 		$('.rules-box').animate({ marginTop: '4vh', opacity:'1' }, 750);
 
@@ -39,9 +52,22 @@ $(document).ready(function(){
 		setTimeout( function(){
 		$('.desc-box').css('display','none');
 		   },500);
+
+		$('.contact-box').animate({ marginTop: '300px', opacity:'0' }, 500);
+		setTimeout( function(){
+		$('.contact-box').css('display','none');
+		   },500);
 	});
 	
 	$(".nav-home").click(function(){
+		$('.nav-desc, .nav-rules, .nav-contact').css({
+			'background-color' : 'rgba(255,255,255,0.7)',
+			'color' : '#000',
+		});
+		$('.nav-home').css({
+			'background-color' : 'rgba(0,0,0,0.7)',
+			'color' : '#fff',
+		});
 		$('.title').css('display', 'flex');
 		$('.title').animate({ marginTop: '0px', opacity:'1' }, 750);
 
@@ -49,23 +75,35 @@ $(document).ready(function(){
 		setTimeout( function(){
 		$('.desc-box').css('display','none');
 		   },1000);
-
 		$('.rules-box').animate({ marginTop: '300px', opacity:'0' }, 500);
 		setTimeout( function(){
 		$('.rules-box').css('display','none');
 		   },1000);
+		$('.contact-box').animate({ marginTop: '300px', opacity:'0' }, 500);
+		setTimeout( function(){
+		$('.contact-box').css('display','none');
+		   },1000);
 	});
 
-	$titlewidth = ($(".title").width()) / 3 ; // what I actually  want is width of .title-box but it is coming out to be 0 by this method: flaw in .width()
-	console.log($titlewidth);
-	$('.nav-home, .nav-desc, .nav-rules').css('min-width', $titlewidth+'px');
-	
+	$(".nav-contact").click(function(){
+		$('.nav-home, .nav-desc, .nav-rules').css({
+			'background-color' : 'rgba(255,255,255,0.7)',
+			'color' : '#000',
+		});
+		$('.nav-contact').css({
+			'background-color' : 'rgba(0,0,0,0.7)',
+			'color' : '#fff',
+		});
+		$('.contact-box').css('display', 'flex');
+		$('.contact-box').animate({ marginTop: '4vh', opacity:'1' }, 750);
 
-	// $('.nav').css('min-width', $titlewidth+'px');
-	// $titlewidth = $titlewidth + 36;
-	// $titlewidth = $titlewidth/3;
-	// var element = document.getElementsByClassName('title')[0];
-	// var positionInfo = element.getBoundingClientRect();
-	// var width = positionInfo.width;
-	// console.log(width);
+		$('.desc-box').animate({ marginTop: '300px', opacity:'0' }, 500);
+		setTimeout( function(){
+		$('.desc-box').css('display','none');
+		   },500);
+		$('.rules-box').animate({ marginTop: '300px', opacity:'0' }, 500);
+		setTimeout( function(){
+		$('.rules-box').css('display','none');
+		   },500);
+	});
 });
